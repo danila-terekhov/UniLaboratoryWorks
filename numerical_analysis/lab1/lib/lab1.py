1e04ea0b46e1de3891ce14bc5ba4f20dd642c063
Poiza <poiza@list.ru>
2020-09-13 16:49:31 +0300

    change algo

--- numerical_analysis/lab1/lib/lab1.py
+++ numerical_analysis/lab1/lib/lab1.py
@@ -46,37 +46,42 @@ class Matrix:
         for i in range(1,n-1):
             R = self.b[i]**-1 ; self.b[i] = 1.0
             self.c[i] *= R ; self.p[i] *= R ; self.q[i] *= R ; self.f[i] *= R
-            R = self.a[i+1] ; self.a[i+1] = 0.0
+            R = self.a[i+1] ;# self.a[i+1] = 0.0
+            self.a[i+1] -= R*self.b[i]
             self.b[i+1] -= R*self.c[i]
             self.f[i+1] -= R*self.f[i]
             self.p[i+1] -= R*self.p[i]
             self.q[i+1] -= R*self.q[i]
 
-        for i in range(n-2, -1, -1):
-            R = self.b[i]**-1 ; self.b[i] = 1.0
-            self.a[i] *= R ; self.p[i] *= R ; self.q[i] *= R ; self.f[i] *= R
-            R = self.c[i-1] ; self.c[i-1] = 0.0
-            self.b[i-1] -= R*self.a[i]
-            self.f[i-1] -= R*self.f[i]
-            self.p[i-1] -= R*self.p[i]
-            self.q[i-1] -= R*self.q[i]
+        self.c[n-1] = self.q[n-1]
+        #R = self.b[n-3]**-1 ; self.b[n-3] = 1.0
 
-        for i in range(1,n):
-            if self.p[i] != 0:
-                R = self.p[i] ; self.p[i] = 0.0
-                self.f[i] -= R*self.f[0]
-                self.q[i] -= R*self.q[0]
+        print (self.c[n-2], self.q[n-2])
+#        self.p[n-1] /= self.q[n-1] ; self.f[n-1] /= self.q[n-1] ; self.q[n-1] = 1
 
-        self.show()
-        print()
-        R = self.q[n-1]**-1; self.q[n-1] = 1.0
-        self.f[n-1] *= R
-        self.show()
-        print()
-        for i in range(n-2,-1,-1):
-            if self.q[i] != 0:
-                R = self.q[i] ; self.q[i] = 0.0
-                self.f[i] -= R*self.f[n-1]
+#        for i in range(n//2-1, -1, -1):
+#            if self.b[i]:
+#                R = self.b[i]**-1 ; self.b[i] = 1.0
+#                self.a[i] *= R ; self.p[i] *= R ; self.q[i] *= R ; self.f[i] *= R
+#            if self.c[i-1]:
+#                R = self.c[i-1] ; self.c[i-1] = 0.0
+#                self.b[i-1] -= R*self.a[i]
+#                self.f[i-1] -= R*self.f[i]
+#                self.p[i-1] -= R*self.p[i]
+#                self.q[i-1] -= R*self.q[i]
+#
+#        for i in range(1,n):
+#            if self.p[i] != 0:
+#                R = self.p[i] ; self.p[i] = 0.0
+#                self.f[i] -= R*self.f[0]
+#                self.q[i] -= R*self.q[0]
+#
+#        R = self.q[n-1]**-1; self.q[n-1] = 1.0
+#        self.f[n-1] *= R
+#        for i in range(n-2,-1,-1):
+#            if self.q[i] != 0:
+#                R = self.q[i] ; self.q[i] = 0.0
+#                self.f[i] -= R*self.f[n-1]
 
 
 m = Matrix()
