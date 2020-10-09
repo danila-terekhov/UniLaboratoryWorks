d1730b95fad2c737794001865fda39e6693fda1c
Poiza <poiza@list.ru>
2020-10-09 14:01:40 +0300

    nm2 finish main algo?

--- numerical_analysis/lab2.py
+++ numerical_analysis/lab2.py
@@ -17,8 +17,8 @@ def kron(i,j):
 
 kron = lambda x,y: (0,1)[x == y]
 sign = lambda x: (1,-1)[x < 0]
-sqrt = lambda x: x**(1/2)
-my = lambda b,x: 1/sqrt(2*b**2 - 2*b*x)
+sqrt = lambda x: x**(0.5)
+my = lambda b,x: 1/sqrt(2*b*b - 2*b*x)
 
 class Matrix:
 
@@ -34,17 +34,42 @@ class Matrix:
             print(row)
 
     def beta(self,i):
-        answer = sign(-self.A[i][i]) * sqrt(sum([X**2 for X in self.A[i][i:]]))
+        answer = sign(-self.A[i][i]) * sqrt(sum([X*X for X in self.A[i][i:]]))
         return answer
+
+
+    def multiply(self, w):
+        n = self.n
+        B = [[0 for _ in range(n)] for _ in range(n)]
+
+        for j in range(n):
+            summ = 0
+            for k in range(n):
+                summ += w[k] * self.A[k][j]
+            for i in range(n):
+                B[i][j] = self.A[i][j] - 2*w[i]*summ
+
+
+        for row in B:
+            print(row)
+
 
     def calculation(self):
 
         n = self.n
         self.f = [sum([self.A[i][j]*self.x[j] for j in range(n)]) for i in range(n)]
 
-        b = self.beta(0)
-        m = my(b,self.x[0])
-        
+        for i in range(1):
+
+            b = self.beta(0)
+            # check m == 0
+            m = my(b,self.x[0])
+            w = [0.0] * i + self.A[i][i:]
+            self.show()
+            w[i] -= b
+            self.multiply(w)
+
+
 
 
 
