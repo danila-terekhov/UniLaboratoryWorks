330b0528705b912a0d3b338876fe96b248a1890b
Poiza <poiza@list.ru>
2020-10-10 14:48:50 +0300

    nm2 in progress

--- numerical_analysis/lab2.py
+++ numerical_analysis/lab2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import numpy as np
 
 from random import *
 
@@ -33,9 +34,16 @@ class Matrix:
         for row in self.A:
             print(row)
 
+#    def beta(self,i):
+#        answer = sign(-self.A[i][i]) * sqrt(sum([X*X for X in self.A[i][i:]]))
+#        return answer
     def beta(self,i):
-        answer = sign(-self.A[i][i]) * sqrt(sum([X*X for X in self.A[i][i:]]))
-        return answer
+        answer = sign(-self.A[i][i])
+        summ = 0
+        for k in range(i,self.n):
+            summ += self.A[k][i]*self.A[k][i]
+        return answer * sqrt(summ)
+
 
 
     def multiply(self, w):
@@ -46,12 +54,16 @@ class Matrix:
             summ = 0
             for k in range(n):
                 summ += w[k] * self.A[k][j]
+            summ *= 2
             for i in range(n):
-                B[i][j] = self.A[i][j] - 2*w[i]*summ
+                B[i][j] = self.A[i][j] - w[i]*summ
 
 
-        for row in B:
-            print(row)
+    def my(self,b,x):
+        znamenatel = 2*b*b - 2*b*x
+        znamenatel = sqrt(znamenatel)
+        return 1/znamenatel
+
 
 
     def calculation(self):
@@ -61,19 +73,30 @@ class Matrix:
 
         for i in range(1):
 
-            b = self.beta(0)
+#            self.show()
+
+            b = self.beta(i)
             # check m == 0
-            m = my(b,self.x[0])
-            w = [0.0] * i + self.A[i][i:]
-            self.show()
+            m = self.my(b,self.A[i][i])
+            w = [0] * i + self.A[i][i:]
             w[i] -= b
-            self.multiply(w)
+            w = [z*m for z in w]
+
+
+            print(sqrt(sum([qwe*qwe for qwe in w])))
+
+            H = [[0 for _ in range(n)] for _ in range(n)]
+            for I in range(n):
+                for J in range(n):
+                    H[I][J] = kron(I,J) - 2*w[I]*w[J]
+            H1 = np.array(H)
+            W = np.array(w)
+            print(H1)
+            AA = np.array(self.A)
+            print(np.dot(H1,AA))
 
 
 
 
-
-
-
-m = Matrix()
+m = Matrix(3,3)
 m.calculation()
