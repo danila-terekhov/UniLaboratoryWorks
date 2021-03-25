49d4fefebd0ed59c8a93d1c6b856f2cc7d90e454
deterekhov <deterekhov@phoenixit.ru>
2021-03-25 16:13:28 +0300

    fix sql script

--- sql/script.sql
+++ sql/script.sql
@@ -12,25 +12,33 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE `articles` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `title` varchar(150) NOT NULL,
-  `content` blob NOT NULL,
+  `content` text NOT NULL,
   `writer_id` int unsigned DEFAULT NULL,
+  `price` tinyint unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `writer_id` (`writer_id`),
-  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`writer_id`) REFERENCES `articles` (`id`)
+  CONSTRAINT `articles_ibfk_14` FOREIGN KEY (`writer_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `articles` (`id`, `date`, `title`, `content`, `writer_id`, `price`) VALUES
+(1,	'2021-03-25 10:25:16',	'Top news',	'abra cadabra\r\npam pam',	1,	101),
+(4,	'2021-03-25 10:36:43',	'Title for 2 news',	'skdaksjdkjasdkfkjasdfkj',	4,	40),
+(14,	'2021-03-25 13:12:13',	'AbraCadabra',	'CONTENT NEW ',	6,	19),
+(15,	'2021-03-25 13:10:10',	'skjasdskjaskjd',	'qweqw',	6,	0);
 
 DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE `subscriptions` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `writer_id` int unsigned NOT NULL,
   `reader_id` int unsigned NOT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `subscriptions` (`id`, `writer_id`, `reader_id`, `date`) VALUES
+(1,	2,	4,	'2021-03-25 11:52:35');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -46,7 +54,8 @@ INSERT INTO `users` (`id`, `role`, `mail`, `password`, `description`) VALUES
 (1,	'writer',	'first@mail.com',	'09f831d11f4dbabcea21e2442c63a874',	'qwerty!1'),
 (2,	'writer',	'second@mail.com',	'09f831d11f4dbabcea21e2442c63a874',	NULL),
 (3,	'writer',	'third@mail.com',	'09f831d11f4dbabcea21e2442c63a874',	NULL),
-(4,	'reader',	'first@mail.ru',	'6123ed2e27adda05c0b40ce00a99db71',	'qwerty2@'),
-(5,	'reader',	'second@mail.ru',	'6123ed2e27adda05c0b40ce00a99db71',	'');
+(4,	'reader',	'first@mail.ru',	'4cfdc2e157eefe6facb983b1d557b3a1',	'qwerty2@'),
+(5,	'reader',	'second@mail.ru',	'6123ed2e27adda05c0b40ce00a99db71',	''),
+(6,	'writer',	'qwe',	'dc829bf0d79e690c59cee708b527e6b7',	NULL);
 
--- 2021-03-24 13:28:09
+-- 2021-03-25 13:13:07
