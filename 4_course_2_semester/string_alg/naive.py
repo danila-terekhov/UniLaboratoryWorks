c28b5a33c51b7d038a13a2a853036de6c4a73220
deterekhov <deterekhov@phoenixit.ru>
2022-04-03 14:26:57 +0300

    string first understood

---
+++ 4_course_2_semester/string_alg/naive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def naive_search(text: str, substr: str):
+    start_pos = []
+    count = 0
+    for i in range(len(text) - len(substr) + 1):
+        is_substring = True
+        for j in range(len(substr)):
+            count += 1
+            if text[i + j] != substr[j]:
+                is_substring = False
+                break
+
+        if is_substring:
+            start_pos.append(i)
+    print(count)
+    return start_pos
+
+
+example = 'aaabaaaa'
+word = 'aaa'
+print(naive_search(example, word))
